65761dafbfef75d1299f2ca353a75ae3a597c933
miguelcgmarques <mike.loud9@gmail.com>
2016-07-02 19:05:37 +0100

    Create skills.py

---
+++ skills.py
@@ -0,0 +1,19 @@
+other_target = ['Aegis',
+				'Mend Condition',
+				'Restore Condition',
+				'Aura Of Stability',
+				'Convert Hexes',
+				'Draw Conditions',
+				'Dwayna\'s Kiss',
+				'Gift Of Health',
+				'Infuse Health',
+				'Spotless Mind',
+				'Spotless Soul',
+				]
+
+no_target = ['Dash',
+			 'Dark Escape',
+			 'Balanced Stance',
+			 'Disciplined Stance',
+			 'Wary Stance'
+			 ]
